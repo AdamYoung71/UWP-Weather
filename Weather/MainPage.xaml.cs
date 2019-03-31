059ec9a52bf9ad09cc222fcf1814d61a2d42e5fa
Adam_Young <30595651+AdamYoung71@users.noreply.github.com>
2019-03-31 21:21:14 +0800

    更新
    1. 增加了图标的图片
    2. 增加了获取本机地理位置的功能（未完成）
    3. 增加了错误处理机制（当输入的地址无查询结果时显示错误信息）

--- Weather/MainPage.xaml.cs
+++ Weather/MainPage.xaml.cs
@@ -23,15 +23,16 @@ namespace Weather
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        public MainPage()
+        public  MainPage()
         {
             this.InitializeComponent();
+
+             
         }
-        
-        private async void AutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+
+        private  void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            /**************实例化API****************/
-            string cityName = args.QueryText;
+            /*string cityName = "成都";
             var myWeather = await MainAPI.MainAPI.GetWrather(cityName);     //实例化主要天气API
             var mySuggestion = await LifeSuggestionAPI.LifeSuggestionAPI.GetSuggestion(cityName);//实例化生活指数
             var myLocation = await CityToLocation.CityToLocation.GetLocation(cityName);     //使用高德API将城市名转换为经纬度。
@@ -40,14 +41,58 @@ namespace Weather
             double Lon = Convert.ToDouble(Locations[1]);
             var myCurrentWeather = await ProCurrentWeather.ProCurrentWeather.GetProCurrentWeather(Lon, Lat);//实例化高级当前天气API
             var myForecast = await ProForecast.ProForecast.GetProForecast(Lon, Lat);//实例化高级天气预报API
-            /*************************************/
 
             CurrentTemp.Text = myWeather.results[0].now.temperature + "℃";
+            CityName.Text = "成都";
 
             var weatherCode = "/Assets/WeatherIcons/" + Convert.ToString(myWeather.results[0].now.code) + ".png";
-            WeatherImg.Source = new BitmapImage(new Uri(WeatherImg.BaseUri, weatherCode));
+            WeatherImg.Source = new BitmapImage(new Uri(WeatherImg.BaseUri, weatherCode));*/
+        }
+
+        private async void AutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+            try
+            {
+                string cityName = args.QueryText;
+                var myWeather = await MainAPI.MainAPI.GetWrather(cityName);     //实例化主要天气API
+                var mySuggestion = await LifeSuggestionAPI.LifeSuggestionAPI.GetSuggestion(cityName);//实例化生活指数
+                var myLocation = await CityToLocation.CityToLocation.GetLocation(cityName);     //使用高德API将城市名转换为经纬度。
+                string[] Locations = myLocation.geocodes[0].location.Split(",");        //将用逗号隔开的经纬度分割分别存入。
+                double Lat = Convert.ToDouble(Locations[0]);
+                double Lon = Convert.ToDouble(Locations[1]);
+                var myCurrentWeather = await ProCurrentWeather.ProCurrentWeather.GetProCurrentWeather(Lon, Lat);//实例化高级当前天气API
+                var myForecast = await ProForecast.ProForecast.GetProForecast(Lon, Lat);//实例化高级天气预报API
+                                                                                        /*************************************/
+
+                CurrentTemp.Text = myWeather.results[0].now.temperature + "℃";
+                CityName.Text = cityName;
+
+                var weatherCode = "/Assets/WeatherIcons/" + Convert.ToString(myWeather.results[0].now.code) + ".png";
+                WeatherImg.Source = new BitmapImage(new Uri(WeatherImg.BaseUri, weatherCode));
+            }
+
+            catch
+            {
+                CityName.Text = "错误！";
+                var weatherCode = "Assets/WeatherIcons/99.png";
+                WeatherImg.Source = new BitmapImage(new Uri(WeatherImg.BaseUri, weatherCode));
+                CurrentTemp.Text = " ";
+            }
+            /**************实例化API****************/
+            
+            
+        }
+
+        private void AutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            if (string.IsNullOrEmpty(sender.Text))
+            {
+
+            }
         }
 
         
+
+       
     }
 }
